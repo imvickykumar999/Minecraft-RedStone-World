cdde61bda7fe23677deb85c522373b66bf1fd6e9
imvickykumar999 <imvickykumar999@gmail.com>
2023-08-02 16:00:56 +0530

    mcpi shifted to redstone repo

--- Commands/Client Server/Client/main.py
+++ Commands/Client Server/Client/main.py
@@ -1,2 +1,28 @@
 
 # pip install mcrcon
+
+from mcrcon import MCRcon as r
+with r('localhost', 'insertyourpasswordhere') as mcr:
+    resp = mcr.command('/list')
+print(resp) #there are 0/20 players online: - This will be different for you.
+
+# -----------------------
+
+# import subprocess, os
+
+# def server_command(cmd):
+#     process.stdin.write(cmd+"\n") #just write the command to the input stream
+
+# process = None
+# executable = '"C:\Program Files\Java\jre1.8.0_111\bin\java.exe" -Xms4G -Xmx4G -jar craftbukkit-1.10.2.jar nogui java'
+
+# while True:
+#     command=input()
+#     command=command.lower()
+#     if process is not None:
+#         if command==("start"):
+#             os.chdir(minecraft_dir)
+#             process = subprocess.Popen(executable, stdin=subprocess.PIPE)
+#             print("Server started.")
+#     else:
+#         server_command(command)
--- Commands/Python Reading/commands/main.py
+++
@@ -1,9 +0,0 @@
-
-# pip install mcpi
-from mcpi.minecraft import Minecraft 
-
-mc = Minecraft.create(address = 'localhost', port = 56531) 
-mc.postToChat('Hello World')
-
-# pos = mc.player.getPos()
-# print(pos)
