c3c12e8495abffb8ec57d06f50093eb505b44315
imvickykumar999 <imvickykumar999@gmail.com>
2023-08-27 13:01:02 +0530

    music system generator

--- Worlds/note block/WriteMCA.py
+++ Worlds/note block/WriteMCA.py
@@ -1,18 +1,28 @@
+
+# https://pypi.org/project/anvil-parser/
 import anvil
-from random import choice
 
 # Create a new region with the `EmptyRegion` class at 0, 0 (in region coords)
 region = anvil.EmptyRegion(0, 0)
 
 # Create `Block` objects that are used to set blocks
-stone = anvil.Block('minecraft', 'stone')
-dirt = anvil.Block('minecraft', 'dirt')
+note_block = anvil.Block('minecraft', 'note_block')
+repeater = anvil.Block('minecraft', 'repeater')
 
-# Make a 16x16x16 cube of either stone or dirt blocks
-for y in range(16):
-    for z in range(16):
-        for x in range(16):
-            region.set_block(choice((stone, dirt)), x, y, z)
+stone = anvil.Block('minecraft', 'stone')
+oak_button = anvil.Block('minecraft', 'oak_button')
+
+for z in range(1, 20):
+    region.set_block(stone, 1, 79, z)
+
+for z in range(1, 21, 2):
+    region.set_block(note_block, 1, 80, z)
+
+for z in range(0, 20, 2):
+    region.set_block(repeater, 1, 80, z)
+
+region.set_block(oak_button, 1, 80, 0)
+# anvil.errors.OutOfBoundsCoordinates: Block (1, 80, -1) is not inside this region
 
 # Save to a file
-region.save('r.0.0.mca')
+region.save('Demo_World/region/r.0.0.mca')
